ef928abaf37435ceea4d1ea2ed03f2e0daae3fbc
Daniel Hernando Salazar Soto <danieltrabajoo2020@gmail.com>
2023-08-30 22:37:56 -0500

    se añade querystring

--- handler.js
+++ handler.js
@@ -1,20 +1,22 @@
 'use strict';
+const querystring = require("querystring")
 const mysql=require('mysql');
 const connection=mysql.createConnection({
   host:'proyectofinal-daniel.cywrvdmfoq84.us-east-2.rds.amazonaws.com',
-  user:' ',
+  user:'admin ',
   port:'3306',
-  pasword:' ',
-  database:' ',
+  pasword:'12345678 ',
+  database:' Restaurante-DanielSotoDB',
 });
 
 module.exports.hacerPedido = async (event) => {
+  const pedido = querystring.parse(event [ "body"] )
   return {
     statusCode: 200,
     body: JSON.stringify(
       {
-        message: 'Go Serverless v1.0! Your function executed successfully!',
-        input: event,
+        message: "pedido exitoso",
+        input: 'orden ${pedido.name}',
       },
       null,
       2
