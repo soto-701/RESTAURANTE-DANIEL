3bf8dc1a3057b10266cbbd6968496c587bd3d665
Daniel Hernando Salazar Soto <danieltrabajoo2020@gmail.com>
2023-09-05 20:34:51 -0500

    soluciones

--- handler.js
+++ handler.js
@@ -10,34 +10,64 @@ const connection=mysql.createConnection({
 });
 
 module.exports.hacerPedido = async (event) => {
-  const pedido = querystring.parse(event [ "body"] )
+  const pedido = querystring.parse(event["body"])
+  await new Promise((resolve, reject) => {
+  const queryclient = "CALL insert_pedido(?,?,?,?,?);";
+    connection.query(queryclient,pedido,descripcion, (err, results) => {
+      if (err) {
+        reject(err);
+      } else {
+        resolve(results);
+      }
+    });
+  });
   return {
     statusCode: 200,
     body: JSON.stringify(
       {
-        message: "pedido exitoso",
-        input: 'orden ${pedido.name}',
+        message: "exitoso",
+        cliente_id: pedido.cliente_id,
+        producto_id: pedido.producto_id,
+        cantidad_und: pedido.cantidad_und,
+        valorUnidad: pedido.valorUnidad,
+        valorTotal: pedido.valorTotal,  
       },
       null,
       2
     ),
   };
-
+  connection.end();
   // Use this code if you don't use the http event with the LAMBDA-PROXY integration
   // return { message: 'Go Serverless v1.0! Your function executed successfully!', event };
 };
 
+
 module.exports.obtenerPedido = async (event) => {
+  const pedido = querystring.parse(event["body"])
+  const queryclient = "SELECT * FROM Pedidosrestaurante.pedidos where id=?;";
+  const consulta = await new Promise((resolve, reject) => {
+     connection.query(queryclient,[pedido.id], (err, results) => {
+      if (err) {
+        reject(err);
+      } else {
+        resolve(results);
+      }
+    });
+  });
+  connection.end();
   return {
     statusCode: 200,
     body: JSON.stringify(
       {
-        message: 'Ejemplo de pedido',
-        input: event,
+        message: "exitoso",
+        pedido: consulta,
       },
       null,
       2
     ),
   };
+  
+  // Use this code if you don't use the http event with the LAMBDA-PROXY integration
+  // return { message: 'Go Serverless v1.0! Your function executed successfully!', event };
 };
 
